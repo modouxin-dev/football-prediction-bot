a1bffb73c4961888d9630340936e943bf9379612
奔波霸 <modouxin@gmail.com>
2026-09-25 11:07:16 +0800

    test: 测试桩支持 disable_web_page_preview

--- tests/test_chart.py
+++ tests/test_chart.py
@@ -172,7 +172,7 @@ class FakeQuery:
     async def answer(self, text=None, show_alert=False):
         self.answers.append((text, show_alert))
 
-    async def edit_message_text(self, text, parse_mode=None, reply_markup=None):
+    async def edit_message_text(self, text, parse_mode=None, reply_markup=None, **kwargs):
         pass
 
 
